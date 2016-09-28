62e61aacb86e235e8a2f8a173b34454c88122109
K4milo <kmilo.vanegas@gmail.com>
2016-09-28 06:09:12 -0500

    fix Gonzo script

--- theme-super-ricas/js/script.js
+++ theme-super-ricas/js/script.js
@@ -473,6 +473,25 @@ function getAudio(){
 }
 
 function getEvents(){
+
+	//Gonzo script
+     $('a.menu').click(function() {
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('#menu_p').animate({
+                opacity: 0,
+                margin: '-100% 0 0 0',
+                'z-index': -100
+            }, 900 );           
+        }else{
+            $(this).addClass('active');
+            $('#menu_p').animate({
+                opacity: 1,
+                margin: '30px 0 0 0',
+            }, 900 ).css('z-index', 100);  
+        }
+    });
+	
 	$( ".grabar" ).click(function() {
 		if(ios == true){
 			alert("Lo sentimos, tu dispositivo no soporta grabación en linea.");
