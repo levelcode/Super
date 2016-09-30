585b84643269244724cf6946a9611dae4f6c862f
levelcode <leonardopalomino@MacBook-Pro-de-Leo.local>
2016-09-30 13:50:02 -0500

    Ajustes Textos y fondos widget

--- theme-super-ricas/templates/premios-tpl.php
+++ theme-super-ricas/templates/premios-tpl.php
@@ -7,10 +7,10 @@
 <div class="wrapper_int premios">
 	<div class="box_premios">
         <div class="titulo_premios">
-            <p>Grábate haciendo el <span> <strong>Reto Super Crokantes</strong></span> , compártelo con tus amigos en Facebook, haz que tu video sea el más compartido y podrás ganar un viaje a caño cristales todo incluido* para que lo disfrutes con la persona que quieras *</p>
+            <p>Si tu vídeo es uno de los más votados en una de las 8 semanas del <span> <strong>Reto Super Crokantes</strong></span>, ganarás un premio semanal y quedarás seleccionado para participar en un último reto que podrá llevarte a disfrutar de un viaje a caño Cristales para dos personas con todo incluido*</p>
         </div>
         <div class="titulo_premios_2">
-            <p>Todas las semanas premiaremos el vídeo más votado con Audífonos Plegables e increíbles Anchetas de nuestros productos.</p>
+            <p>Todas las semanas premiaremos el vídeo más votado con Audífonos Plegables e increíbles Anchetas de <span> <strong>nuestros productos.</strong></span></p>
         </div>
         <a href="javascript:void(0);" class="inicia_el_reto"></a>
         <a href="javascript:void(0);" class="terminos_condiciones_premios">
